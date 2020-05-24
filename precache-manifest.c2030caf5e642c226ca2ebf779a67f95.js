617c8aaa34ff40b68d03e688e2b8e4763ebf8c16
Rajat9915 <49584553+Rajat9915@users.noreply.github.com>
2020-05-24 13:15:53 +0530

    Updates

---
+++ precache-manifest.c2030caf5e642c226ca2ebf779a67f95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "574abf2fccca5b7b04c0a8ff3743ab9c",
+    "url": "/expenseTracker/index.html"
+  },
+  {
+    "revision": "cc34f4aaa5d1db0fefd7",
+    "url": "/expenseTracker/static/css/main.f65cf5c3.chunk.css"
+  },
+  {
+    "revision": "cd6cc9d88103b5772f55",
+    "url": "/expenseTracker/static/js/2.ba30053d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expenseTracker/static/js/2.ba30053d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc34f4aaa5d1db0fefd7",
+    "url": "/expenseTracker/static/js/main.9a427d23.chunk.js"
+  },
+  {
+    "revision": "2b90336cb92ba9339c74",
+    "url": "/expenseTracker/static/js/runtime-main.93e0f17b.js"
+  }
+]);
